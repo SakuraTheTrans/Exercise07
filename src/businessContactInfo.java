c48fb8c4af37b1276385f334cff302e4decfd730
Sakura <sakurathetrans@gmail.com>
2025-02-25 13:40:41 -0500

    wooo code

--- src/businessContactInfo.java
+++ src/businessContactInfo.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class businessContactInfo extends contactInfo{
+    private String phoneNum;
 
-public class businessContactInfo {
+    public businessContactInfo(String name, String email, String phoneNum) {
+        super(name, email);
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    @Override
+    public void displayContact() {
+        System.out.println("Name : " + this.getName() + "\nEmail : " + this.getEmail() + "\nPhone : " + this.getPhoneNum() + "\n");
+    }
 }
--- src/contactInfo.java
+++ src/contactInfo.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class contactInfo {
+    private String name;
+    private String email;
+
+    public contactInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "contactInfo{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void displayContact(){
+        System.out.println("Name : " + this.getName() + "\nEmail : " + this.getEmail() + "\n");
+    }
 }
